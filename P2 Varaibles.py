9890c84ce35ef610f986e40c0cab259bcfe99656
IcarlyRD <elaine.labuschagne@kingsway.school.nz>
2025-05-21 09:49:02 +1200

    Add files via upload
    How do make a variable

---
+++ P2 Varaibles.py
@@ -0,0 +1,15 @@
+# Code to make varaibles 
+#Author Elaine
+# Date 16/5/2025
+#Version 2
+
+#Varaibles can be a String (use speech marks)
+greeting="Hello word"
+print(greeting)
+
+#Varaibles can be numbers
+Fav_num=7
+num_string= "5"
+
+print(Fav_num * 2)
+print (num_string *5)
