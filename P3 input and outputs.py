25cd7766253f9cd35b1b5a3f3815e042adc5701c
IcarlyRD <elaine.labuschagne@kingsway.school.nz>
2025-05-21 10:15:10 +1200

    Add files via upload
    How to do inputs and outputs

---
+++ P3 input and outputs.py
@@ -0,0 +1,27 @@
+# How to use inputs and outputs
+# Author Elaine
+# Date 21/5/2025
+# Version 1
+
+# Ask the user  for their name 
+username= input (" What is your name? ")
+
+#Ask the user for their favourite number (integer)
+fav_num= int(input("What is your favourite number? "))
+
+# Double, halve and square the user's favourite number
+Double =fav_num * 2
+halve= fav_num/2
+square= fav_num*fav_num
+
+#Great the user 
+print(f"Hi {username}, your favourite number isElaine {fav_num} ")
+
+
+#Output and results of doubling, halving and
+#squaring their favourite integer
+print(f"Double{fav_num} is {Double}.")
+print (f"Half {fav_num} is {halve}.")
+print(f"{fav_num} squared is {square}.")
+print()
+print("Have  a nice day")
